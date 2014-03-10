50b67f0eac3a079a5fbb73897a0d8a04f95d4a4a
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-10 17:52:17 -0400

    ...

--- dev/test.php
+++ dev/test.php
@@ -4,13 +4,8 @@ include_once '../autoloader.php';
 
 use DFePhp\MakeDFe;
 
-$test = new MakeDFe('NFe500', array());
+$test = new MakeDFe('NFe500');
 
 $test->test();
-//print_r($test->test());
-
-//echo getcwd() . "<br>";
-//$test2 = explode(DIRECTORY_SEPARATOR, dirname(__FILE__));
-
 
 
--- src/DFePhp/MakeDFe.class.php
+++ src/DFePhp/MakeDFe.class.php
@@ -13,6 +13,22 @@ namespace DFePhp;
  * Classe para construir o Documento Fiscal Eletrônico.
  */
 class MakeDFe {
+
+  /**
+   * Tipo da entrada de dados. Arquivo TXT ou XML.
+   */
+  const TIPO_INPUT_ARQUIVO = 1;
+
+  /**
+   * Tipo da entrada de dados. Array estruturada.
+   */
+  const TIPO_INPUT_ARRAY = 2;
+
+  /**
+   * Tipo de entrada de dados. 
+   */
+  private $tipo_de_input;
+
   /**
    * Layout do DFe a ser gerado. 
    */
@@ -34,13 +50,30 @@ class MakeDFe {
   private $txt_do_dfe;
 
   /**
+   * Caminho físico da biblioteca.
+   */
+  private $path_dos_arquivos_dfe;
+
+  /**
+   * Nome do arquivo que contém/conterá o DFe.
+   */
+  private $nome_do_arquivo;
+
+  /**
+   * Define a versão do layout da estrutura de dados do DFe.
    * 
    * @param String $versao_do_layout
    *   Versão do Layout para gerar o DFe.
-   * @param Array $input
-   *   Veja a descrição do método dfe_data_input().
+   *
+   * @param Array/String $input_data
+   *   Opcional:
+   *   String | Nome do arquivo que contém a DFe.
+   *   Array | Os dados da DFe.
+   *
+   * @param String $path_dos_arquivos_dfe
+   *   Opcional: O caminho físico da pasta onde os arquivos DFe estão/serão armazenados.
    */
-  public function __construct($versao_do_layout = '', $input_data = '') {
+  public function __construct($versao_do_layout = '', $input_data = '', $path_dos_arquivos_dfe = '') {
     try {
       $exception_error_message = FALSE;
 
@@ -66,8 +99,38 @@ class MakeDFe {
       echo $e->getMessage();
     }
 
-    if (!empty($input)) {
-      $this->dfe_data_input($input_data);
+    $this->input_datainput_data($input_data, $path_dos_arquivos_dfe);
+  }
+
+  /**
+   *
+   */
+  private function set_input_data($input_data = '') {
+    
+    if (!empty($input_data)) {
+      $this->input_data = $input_data;
+    }
+
+    if (is_string($this->input_data)) {
+      $this->nome_do_arquivo = $input_data;
+      $this->tipo_de_input = self::TIPO_INPUT_ARQUIVO;
+    }
+    else {
+      $this->array_do_dfe = $input_data;
+      $this->tipo_de_input = self::TIPO_INPUT_ARRAY;
+    }
+
+  }
+
+  /**
+   *
+   */
+  private function set_path_dos_arquivos_dfe($path_dos_arquivos_dfe = '') {
+    // Define o Path padrão.
+    $this->path_dos_arquivos_dfe = $this->get_path_to('arquivosDfe' . DIRECTORY_SEPARATOR . 'txts');
+
+    if (!empty($path_dos_arquivos_dfe)) {
+      $this->path_dos_arquivos_dfe = $path_dos_arquivos_dfe;
     }
   }
 
@@ -88,13 +151,11 @@ class MakeDFe {
    * @param String $path
    *   Caminho físico do diretório onde o arquivo está arquivado.
    */
-  private function transforma_txt2array($nome_do_arquivo, $path = '') {
-    // Define o Path padrão.
-    if (empty($path)) {
-      $path = $this->get_path_to('arquivosDfe' . DIRECTORY_SEPARATOR . 'txts');
-    }
+  private function transforma_txt2array() {
+    $path_dos_arquivos_dfe = $this->path_dos_arquivos_dfe;
+    $nome_do_arquivo = $this->nome_do_arquivo;
 
-    $handle = fopen($path . DIRECTORY_SEPARATOR . $nome_do_arquivo, "r");
+    $handle = fopen($path_dos_arquivos_dfe . DIRECTORY_SEPARATOR . $nome_do_arquivo, "r");
     if ($handle) {
       $txt = array();
       $primeiro_loop = TRUE;
@@ -166,10 +227,33 @@ class MakeDFe {
   }
 
   /**
-   * 
+   *
+   *
+   *
+   * @param String $tipo_do_input
+   *   array: A entrada de dados é uma array, assim será gerado o XML e o TXT.
+   *   arquivo: A entrada de dados é um arquivo ( TXT ou XML ).
    */
-  public function dfe_data_input($input_data) {
-    
+  public function entrada_de_dados($input_data, $path_dos_arquivos_dfe = '') {
+
+    switch($this->tipo_de_input) {
+      case self::TIPO_INPUT_ARQUIVO:
+        $extensao = explode('.', $input_data);
+        $extensao = strtolower($extensao[1]);
+  
+        switch($extensao) {
+          case 'txt':
+            $this->transforma_txt2array();
+          break;
+          case 'xml':
+            $this->transforma_txt2xml();
+          break;
+        }
+      break;
+      case self::TIPO_INPUT_ARRAY:
+        
+      break;
+    }
   }
 
 
@@ -178,10 +262,10 @@ class MakeDFe {
 
     echo "<pre>";
 
-    $arquivo = 'NOTAFISCAL.txt';
+    $nome_do_arquivo = 'REGISTROSCTE.txt';
 
-    $this->transforma_txt2array($arquivo);
-    
+    $this->transforma_txt2array($nome_do_arquivo);
+
     print_r($this->txt_do_dfe);
 
 
