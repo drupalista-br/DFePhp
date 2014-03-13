f97fafaeeae4e70a2dfca1e68d6f2f75bb90d5b5
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-13 16:08:17 -0400

    Instalado Symphony e Composer. Adicionado PHPUnit Test.

---
+++ src/Test/MakeDFeTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DFePhp\Test;
+
+class MakeDFeTest extends \PHPUnit_Framework_TestCase {
+  public function testTrueIsTrue(){
+    $foo = true;
+    $this->assertTrue($foo);
+  }
+}
