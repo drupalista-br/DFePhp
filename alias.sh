63fe55cef5529045c9355c00a3be4e6bf2054371
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-14 12:28:27 -0400

    Renomeado

---
+++ alias.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+alias t="phpunit"
+alias s="git status"
+alias u="git push origin v0.x.x"
+alias d="git pull origin v0.x.x"
+alias a="git add . "
+
+c() { git commit -m $@; }
+r() { git rm $@;}
