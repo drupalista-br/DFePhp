65590104ad115dd4ff3344883bc4092b8a734403
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-14 12:32:01 -0400

    ...

--- alias.sh
+++ alias.sh
@@ -5,5 +5,5 @@ alias u="git push origin v0.x.x"
 alias d="git pull origin v0.x.x"
 alias a="git add . "
 
-c() { git commit -m $@; }
+c() { git commit -m "$@"; }
 r() { git rm $@;}
--- aliases.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-alias t="phpunit"
-alias gs="git status"
-alias gu="git push origin v0.x.x"
-alias gd="git pull origin v0.x.x"
-alias ga="git add ."
-alias gc="git commit -m "
