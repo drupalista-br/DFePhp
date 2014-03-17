62c317821bb068d9aba2535e4cc8ff20b09a2eb9
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-17 12:16:18 -0400

    ...

--- dev/test.php
+++ dev/test.php
@@ -5,74 +5,50 @@ include_once '../autoloader.php';
 use DFePhp\MakeDFe;
 header('Content-Type: text/html; charset=utf-8');
 echo '<pre>';
-$test = new MakeDFe('NFe500');
+
+/**
+ * Load, update, and save an XML document
+ *
+ * sudo apt-get install php-pear
+ * /
+try {
+   $xmldas = SDO_DAS_XML::create("/opt/lampp/htdocs/sites/saturnopecas.com.br/libraries/DFePhp/src/DFePhp/Schemas/NFe/PL008bXsd310/nfe_v3.10.xsd");
+   $document = $xmldas->loadFile("letter.xml");
+   $root_data_object = $document->getRootDataObject();
+   /*$root_data_object->date = "September 03, 2004";
+   $root_data_object->firstName = "Anantoju";
+   $root_data_object->lastName = "Madhu";* /
+   $xmldas->saveFile($document, "letter-out.xml");
+   echo "New file has been written:\n";
+   print file_get_contents("letter-out.xml");
+} catch (SDO_Exception $e) {
+   print($e->getMessage());
+}*/
+
+
+/*$test = new MakeDFe('NFe500');
 
 $test->set_input_path('/opt/lampp/htdocs/sites/saturnopecas.com.br/libraries/DFePhp/dev/tests');
 $test->set_input_nome_do_arquivo('dfe.xml');
-$test->carrega_dados_do_arquivo();
+$test->carrega_dados_do_arquivo();*/
 
 
-$doc = new DOMDocument();
-$doc->load('book.xml');
-echo $doc->saveXML();
+$doc = '/opt/lampp/htdocs/sites/saturnopecas.com.br/libraries/DFePhp/src/DFePhp/Schemas/NFe/PL008bXsd310/leiauteNFe_v3.10.xsd';
+
+$xml = simplexml_load_file($doc);
+
+echo "Obtain all element names incl. complexTypes:\n";
+
+$elementNames = array_map('strval', $xml->xpath('//xs:element/@name'));
+print_r($elementNames);
+
+echo "\nObtain all element names excl. complexTypes and those
+  which contain anything incl. comments, text etc.:\n";
+
+$elementNames = array_map('strval', $xml->xpath('//xs:element[not(node())]/@name'));
+print_r($elementNames);
 
 
-/*try {
-  $test->converte_txt2array();
-}
-catch(\Exception $e) {
-  echo $e->getMessage();
-}*/
+//print_r($doc);
 
-/*
-$input_nome_do_arquivo = 'test.TXT';
-$file = strtolower(pathinfo($input_nome_do_arquivo, PATHINFO_EXTENSION));
-print_r($file);
-//print_r($test);
-//$test->test();* /
-
-
-
-function libxml_display_error($error)
-{
-    $return = "<br/>\n";
-    switch ($error->level) {
-        case LIBXML_ERR_WARNING:
-            $return .= "<b>Warning $error->code</b>: ";
-            break;
-        case LIBXML_ERR_ERROR:
-            $return .= "<b>Error $error->code</b>: ";
-            break;
-        case LIBXML_ERR_FATAL:
-            $return .= "<b>Fatal Error $error->code</b>: ";
-            break;
-    }
-    $return .= trim($error->message);
-    if ($error->file) {
-        $return .=    " in <b>$error->file</b>";
-    }
-    $return .= " on line <b>$error->line</b>\n";
-
-    return $return;
-}
-
-function libxml_display_errors() {
-    $errors = libxml_get_errors();
-    foreach ($errors as $error) {
-        print libxml_display_error($error);
-    }
-    libxml_clear_errors();
-}
-
-// Enable user error handling
-libxml_use_internal_errors(true);
-
-$xml = new DOMDocument(); 
-$xml->load('example.xml'); 
-
-if (!$xml->schemaValidate('example.xsd')) {
-    print '<b>DOMDocument::schemaValidate() Generated Errors!</b>';
-    libxml_display_errors();
-}
-*/
 
