583159dedc59eb3ea5e055185e6f3cd7831daa83
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2014-03-20 12:25:24 -0400

    Feito cópia do leiauteNFe_v3.10.xsd p/ dev

--- dev/test.php
+++ dev/test.php
@@ -16,24 +16,34 @@ $test->carrega_dados_do_arquivo();*/
 
 // http://stackoverflow.com/questions/16098414/how-to-read-an-array-of-element-names-from-xmlschema-xml-file-in-php
 
-$doc = '/var/www/sites/saturnopecas.com.br/libraries/DFePhp/src/DFePhp/Schemas/NFe/PL008bXsd310/leiauteNFe_v3.10.xsd';
+$doc = realpath(__DIR__ . '/../src/DFePhp/Schemas/NFe/PL008bXsd310/leiauteNFe_v3.10.xsd');
 
-$xml = simplexml_load_file($doc);
+$xsd = simplexml_load_file('leiauteNFe_v3.10.xsd');
+//$xsd->registerXPathNamespace('xs', 'http://www.portalfiscal.inf.br/nfe');
+
+print_r(get_class_methods($xsd));
+print_r($xsd->getDocNamespaces());
+
+$xsd->registerXPathNamespace('xs', 'http://www.w3.org/2001/XMLSchema');
 
 echo "Obtain all element names incl. complexTypes:\n";
 
-// $elementNames = array_map('strval', $xml->xpath('//xs:element/@name'));
+// $elementNames = array_map('strval', $xsd->xpath('//xs:element/@name'));
 
 //print_r($elementNames);
 
 echo "\nObtain all element names excl. complexTypes and those
   which contain anything incl. comments, text etc.:\n";
 
-$elementNames = $xml->xpath("(./xs:complexType)[1]/xs:annotation/xs:documentation");
+$elementNames = $xsd->xpath("./xs:complexType[1]/xs:annotation/xs:documentation");
+// $elementNames = $xsd->xpath("./xs:complexType[1]/xs:annotation/xs:documentation[text()]");
+// $elementNames = $xsd->xpath("./xs:complexType[1]/xs:annotation/xs:documentation/text()");
 
 print_r($elementNames);
 
+print_r(get_class_methods($elementNames[0]));
 //print_r($doc);
+
 
 /*
 complexTypes
